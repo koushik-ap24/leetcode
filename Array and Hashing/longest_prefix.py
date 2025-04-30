41582f6a085b20937c671bd9c263f89119f8c183
PHOENIX-DESKTOP\koush <koushik.prabu@gmail.com>
2025-04-30 20:02:53 +1000

    Restructuring based on problem type

---
+++ Array and Hashing/longest_prefix.py
@@ -0,0 +1,35 @@
+from typing import List
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        # Use scanning
+        if len(strs) == 1:
+            return strs[0]
+        
+        longestPrefix = strs[0]
+        for i in range(1, len(strs)):
+            str_1 = strs[i]
+            prefix = ""
+            for j in range((min(len(str_1), len(longestPrefix)))):
+                if str_1[j] == longestPrefix[j]:
+                    prefix += longestPrefix[j]
+                else:
+                    break
+            
+            longestPrefix = prefix
+        
+        return longestPrefix
+
+
+        ## Better solution, only compare first and last string after sorting list
+
+        ans=""
+        v=sorted(v)
+        first=v[0]
+        last=v[-1]
+        for i in range(min(len(first),len(last))):
+            if(first[i]!=last[i]):
+                return ans
+            ans+=first[i]
+        return ans 
