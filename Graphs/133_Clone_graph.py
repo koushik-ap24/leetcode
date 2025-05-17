fb5e8505bdd44e0f170577f7ccf7a256665b78c5
PHOENIX-DESKTOP\koush <koushik.prabu@gmail.com>
2025-05-17 17:04:41 +1000

    soln for 133 - clone graph

--- Graphs/133_Clone_graph.py
+++ Graphs/133_Clone_graph.py
@@ -1,11 +1,11 @@
-"""
+
 # Definition for a Node.
 class Node:
     def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
-"""
 
+from collections import deque
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
