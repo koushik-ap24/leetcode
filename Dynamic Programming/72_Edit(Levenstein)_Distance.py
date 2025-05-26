841d83a2b94c3e7edc769e98d663113a776dd7bd
PHOENIX-DESKTOP\koush <koushik.prabu@gmail.com>
2025-05-26 19:01:10 +1000

    bottom up soln to edit distance

--- Dynamic Programming/72_Edit(Levenstein)_Distance.py
+++ Dynamic Programming/72_Edit(Levenstein)_Distance.py
@@ -24,3 +24,20 @@ class Solution:
             return cache[(i,j)]
         
         return dfs(0,0)
+
+        # Bottom up -> consider empty strings as base case and do the same operations as top-down but in reverse!
+        dp = [[0] * (n+1) for _ in range(m+1)]
+        for i in range(n+1):
+            dp[0][i] = i
+        
+        for i in range(m+1):
+            dp[i][0] = i
+        
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if word2[i-1] == word1[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = 1 + min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
+        
+        return dp[m][n]
