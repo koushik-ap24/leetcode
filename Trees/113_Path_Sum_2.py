fdf3f012fec3b5a239c5725e6a1365cf6f7bf6ba
PHOENIX-DESKTOP\koush <koushik.prabu@gmail.com>
2025-09-26 15:17:44 +1000

    Solution to path sum 2

---
+++ Trees/113_Path_Sum_2.py
@@ -0,0 +1,29 @@
+from typing import Optional,List
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        res = []
+        
+        def dfs(node, target, path):
+            if not node:
+                return
+            
+            path.append(node.val)
+            
+            if not node.left and not node.right:
+                if node.val == target:
+                    res.append(path.copy())
+
+            dfs(node.left, target-node.val, path)
+            dfs(node.right, target-node.val, path)
+            path.pop()
+        
+        dfs(root, targetSum, [])
+        return res
